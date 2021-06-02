3c5f13b22bb8262d78ced1e871aa238be9e3de56
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-02 19:26:14 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/components/frontend.sh
+++ shell-scripting/components/frontend.sh
@@ -2,7 +2,7 @@
 source components/common.sh
 HEAD "Installing nginx"
 
-yum install nginx -y &>> /tmp/roboshop.log
+#yum install nginx -y &>> /tmp/roboshop.log
 
-systemctl enable nginx >> /tmp/roboshop.log
-systemctl start nginx >> /tmp/roboshop.log
+#systemctl enable nginx >> /tmp/roboshop.log
+#systemctl start nginx >> /tmp/roboshop.log
