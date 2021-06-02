d9bd465ee91cb170e3498d3fd0c01981c6d8dec3
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-02 19:36:10 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/components/common.sh
+++ shell-scripting/components/common.sh
@@ -4,8 +4,8 @@ HEAD(){
 
 STAT() {
   if [ $1 -eq 0 ]; then
-    echo -e "\e[32m Done \e[0m"
+    echo -e "\e[32m ---- Done \e[0m"
   else
-      echo -e "\e[31m Failed \e[0m"
+      echo -e "\e[31m ----Failed \e[0m"
   fi
 }
