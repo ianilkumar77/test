d44660414d00dd4229c3cf0e4b4ee098e766811c
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-03 22:28:37 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/instance-create/launch.sh
+++ shell-scripting/instance-create/launch.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 COMPONENT=$1;
+
+if [ -z "${COMPONENT}" ]; then
+  echo "atleast one component name is required"
+  exit 1
+
+fi
 LAUNCH_TEMPLATE_ID=lt-08bf8a3b7c53f50f8
 VERSION=1
 aws ec2 run-instances --launch-template LaunchTemplateId=${LAUNCH_TEMPLATE_ID},Version=${VERSION} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
