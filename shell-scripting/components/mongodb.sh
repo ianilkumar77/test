2a632e5f657722fb6367aff07a793d46eb03b19d
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-03 17:31:10 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/components/mongodb.sh
+++ shell-scripting/components/mongodb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source components/common.sh
 
-HEAD "Setting up MongoDB Repository
+HEAD "Setting up MongoDB Repository"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' > /etc/yum.repos.d/mongodb.repo
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' > /etc/yum.repos.d/mongodb.repo &>> /tmp/roboshop.log
 STAT $?
 
 HEAD "Install MongoDB"
