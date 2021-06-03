818df5f7035063fe916a966fc2cf556972c8fae5
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-03 17:18:53 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/components/mongodb.sh
+++ shell-scripting/components/mongodb.sh
@@ -9,3 +9,38 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo"  &>> /tmp/roboshop/log
 STAT $?
 
+HEAD "Install MongoDB"
+yum install -y mongodb-org &>> /tmp/roboshop.log
+STAT $?
+
+HEAD "Update IP Address in /etc/mongodb.conf"
+sed -e -n 's/127.0.0.1/0.0.0.0' /etc/mongodb.conf  &>> /tmp/roboshop.log
+STAT $?
+
+HEAD "Download the Mongo DB Application"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>> /tmp/roboshop.log
+STAT $?
+
+HEAD "Download the Mongo DB Application"
+unzip mongodb.zip -d /tmp &>> /tmp/roboshop.log
+STAT $?
+
+HEAD "Creating MongoDB Schema"
+cd mongodb-main  && mongo < catalogue.js && mongo < users.js  &>> /tmp/roboshop.log
+STAT $?
+
+
+
+
+
+
+HEAD "Enable  MongoDB service"
+systemctl enable mongod   &>> /tmp/roboshop.log
+STAT $?
+
+HEAD "Start  MongoDB service"
+systemctl start mongod   &>> /tmp/roboshop.log
+STAT $?
+
+
+
