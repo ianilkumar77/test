cb2f81a9b54d49004b45fcf51ddc0e725a9c0889
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-03 16:30:19 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/components/frontend.sh
+++ shell-scripting/components/frontend.sh
@@ -12,3 +12,20 @@ STAT $?
 HEAD "Enabling Nginx"
 systemctl start nginx &>> /tmp/roboshop.log
 STAT $?
+
+HEAD "Downloading the frontend application"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> /tmp/roboshop.log
+STAT $?
+
+HEAD "Deploying the frontend application"
+cd /usr/share/nginx/html && rm -rf * &>> /tmp/roboshop.log
+STAT $?
+unzip /tmp/frontend.zip
+STAT $?
+mv frontend-main/* . && mv static/* . && rm -rf frontend-master README.md &>> /tmp/roboshop.log
+STAT $?
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> /tmp/roboshop.log
+STAT $?
+
+
+
