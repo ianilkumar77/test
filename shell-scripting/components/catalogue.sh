0eaecd94324e5b166258e412b0612d8ab43248c0
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-04 18:40:41 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/components/catalogue.sh
+++ shell-scripting/components/catalogue.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
+source components/common.sh
+rm /tmp/roboshop.log
 
-echo Installing catalogue component
+HEAD "Installing nodejs make and gcc-c++"
+yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
+STAT $?
+
+
+HEAD "Creating roboshop user"
+useradd roboshop &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Downloading the Catalogue application from git hub"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Unzipping the Catalogue application"
+cd /home/roboshop && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Installing the Catalogue application"
+mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install &>>/tmp/roboshop.log
+STAT $?
+
+
+
