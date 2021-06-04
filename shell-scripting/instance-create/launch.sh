6ba572d21d07a746c8ea9d5f29607be25cfbaea8
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-04 17:13:18 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/instance-create/launch.sh
+++ shell-scripting/instance-create/launch.sh
@@ -5,7 +5,12 @@ COMPONENT=$1;
 if [ -z "${COMPONENT}" ]; then
   echo "component name is required"
   exit 1
+fi
 
+INSTANCE_STATE=aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend"|jq .Reservations[].Instances[].State.Name|xargs -n1
+if [ ${INSTANCE_STATE} = "running" ]; then
+  echo "Instance already created and running"
+  exit 0
 fi
 LAUNCH_TEMPLATE_ID=lt-08bf8a3b7c53f50f8
 VERSION=1
