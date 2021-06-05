b05e2a33b55a90eb89a8acd1c4f73c952063db82
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-05 19:55:31 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/components/catalogue.sh
+++ shell-scripting/components/catalogue.sh
@@ -23,7 +23,7 @@ STAT $?
 FIX_APP_PERMISSION
 
 HEAD "Updating DNS record of mongodb server"
-sed -i -e '/s/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 STAT $?
 
 HEAD "Start systemd & start catalogue service"
