604463dfdf8b69cf12e51d34f2d3980f56a2d9d3
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-06 18:19:31 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/components/redis.sh
+++ shell-scripting/components/redis.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo Installing redis component
+source components/common.sh
+rm -f /tmp/roboshop.log
+set-hostname $1
+
+HEAD "Install redis prerequisite applications" &>> /tmp/roboshop.log
+yum install epel-release yum-utils -y &>> /tmp/roboshop.log && yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>> /tmp/roboshop.log && yum-config-manager --enable remi &>> /tmp/roboshop.log && yum install redis -y &>> /tmp/roboshop.log
+STAT $?
+
+HEAD "Update the BINIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf" &>> /tmp/roboshop.log
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>> /tmp/roboshop.log
+STAT $?
+
+HEAD "Start Redis Database"
+systemctl enable redis && systemctl start redis
+STAT $?
