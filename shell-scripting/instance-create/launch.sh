6e2b0606384ce9fedae6baf46dce616ebbbb3c58
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-07 17:12:55 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/instance-create/launch.sh
+++ shell-scripting/instance-create/launch.sh
@@ -16,7 +16,7 @@ if [ "${INSTANCE_STATE}" = "running" ]; then
     exit 0
 fi
 LAUNCH_TEMPLATE_ID=lt-08bf8a3b7c53f50f8
-VERSION=2
+VERSION=3
 aws ec2 run-instances --launch-template LaunchTemplateId=${LAUNCH_TEMPLATE_ID},Version=${VERSION} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"|jq
 sleep 30
 PRIVATE_IP_ADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].PrivateIpAddress|xargs -n1)
