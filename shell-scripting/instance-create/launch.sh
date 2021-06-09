efa679df40c6e6a71d9fe1329c26005a4bb1fd45
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-09 16:51:26 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/instance-create/launch.sh
+++ shell-scripting/instance-create/launch.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 COMPONENT=$1;
+LAUNCH_TEMPLATE_ID=lt-08bf8a3b7c53f50f8
+VERSION=3
+HOSTZONE_ID=Z0436692C7FU5JVXITRX
 
 if [ -z "${COMPONENT}" ]; then
   echo "component name is required"
   exit 1
 fi
 
+INSTANCE_CREATE() {
 INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].State.Name|xargs -n1)
 if [ "${INSTANCE_STATE}" = "running" ]; then
     echo "Instance already created and running"
@@ -19,6 +23,22 @@ LAUNCH_TEMPLATE_ID=lt-08bf8a3b7c53f50f8
 VERSION=3
 aws ec2 run-instances --launch-template LaunchTemplateId=${LAUNCH_TEMPLATE_ID},Version=${VERSION} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"|jq
 sleep 20
-PRIVATE_IP_ADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].PrivateIpAddress|xargs -n1)
+DNS_UPDATE
+
+}
+
+DNS_UPDATE() {
+  PRIVATE_IP_ADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].PrivateIpAddress|xargs -n1)
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP_ADDRESS}/" record.json > /tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z0436692C7FU5JVXITRX --change-batch file:///tmp/record.json | jq
+aws route53 change-resource-record-sets --hosted-zone-id ${HOSTZONE_ID} --change-batch file:///tmp/record.json | jq
+}
+
+if [ "${1}" == "all"  ]; then
+  for component in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment ; do
+    COMPONENT = ${component}
+    INSTANCE_CREATE
+  done
+  else
+    COMPONENT = $1
+    INSTANCE_CREATE
+fi
