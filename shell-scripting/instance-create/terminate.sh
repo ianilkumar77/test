3c9094abae8d8b8ee0fe4c8ab5d789066f650171
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-09 18:40:50 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/instance-create/terminate.sh
+++ shell-scripting/instance-create/terminate.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-COMPONENT=$1
+if [ "${1}" == "all"  ]; then
+  for component in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment ; do
+    COMPONENT=${component}
+    TERMINATE_INSTANCE
+  done
+  else
+    COMPONENT=$1
+    TERMINATE_INSTANCE
+fi
+
 SPOT_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].SpotInstanceRequestId|xargs -n1)
 if [ -z "${SPOT_INSTANCE_ID}" ]; then
   echo "component name is wrong or empty"
   exit 1
 fi
+
+TERMINATE_INSTANCE() {
 aws ec2 cancel-spot-instance-requests --spot-instance-request-ids ${SPOT_INSTANCE_ID}
 INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].InstanceId|xargs -n1)
 aws ec2 terminate-instances --instance-ids ${INSTANCE_ID}
+}
