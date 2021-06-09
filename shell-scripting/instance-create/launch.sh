38f2c08351f30226d98d12981dcc088a9d862a9b
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-09 16:37:11 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/instance-create/launch.sh
+++ shell-scripting/instance-create/launch.sh
@@ -18,7 +18,7 @@ fi
 LAUNCH_TEMPLATE_ID=lt-08bf8a3b7c53f50f8
 VERSION=3
 aws ec2 run-instances --launch-template LaunchTemplateId=${LAUNCH_TEMPLATE_ID},Version=${VERSION} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"|jq
-sleep 30
+sleep 20
 PRIVATE_IP_ADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].PrivateIpAddress|xargs -n1)
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP_ADDRESS}/" record.json > /tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id Z0436692C7FU5JVXITRX --change-batch file:///tmp/record.json | jq
