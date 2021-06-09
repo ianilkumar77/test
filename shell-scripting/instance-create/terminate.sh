a4a7668206a209cb030d1a3117cd0e7834ddb44a
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-09 17:40:01 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/instance-create/terminate.sh
+++ shell-scripting/instance-create/terminate.sh
@@ -2,4 +2,8 @@
 
 COMPONENT=$1
 SPOT_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].SpotInstanceRequestId|xargs -n1)
+if [ -z "${SPOT_INSTANCE_ID}" ]; then
+  echo "component name is wrong or empty"
+  exit 1
+fi
 aws ec2 cancel-spot-instance-requests --spot-instance-request-ids ${SPOT_INSTANCE_ID}
