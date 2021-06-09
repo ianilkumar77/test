41b43b4dfacdc6a1fda24987730dd02994ae980f
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-09 17:46:29 +0530

    Commiting the frontend main.yaml corrected the package name.

--- shell-scripting/instance-create/terminate.sh
+++ shell-scripting/instance-create/terminate.sh
@@ -7,5 +7,5 @@ if [ -z "${SPOT_INSTANCE_ID}" ]; then
   exit 1
 fi
 aws ec2 cancel-spot-instance-requests --spot-instance-request-ids ${SPOT_INSTANCE_ID}
-INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[]..InstanceId|xargs -n1)
+INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].InstanceId|xargs -n1)
 aws ec2 terminate-instances --instance-ids ${INSTANCE_ID}
