179334cb8f697e5e7f973e90b078ee68e20bc833
aindraka <anil.kumar.indrakanti@oracle.com>
2021-06-10 07:20:45 +0530

    Commiting the frontend main.yaml corrected the package name.

--- ansible/launch.sh
+++ ansible/launch.sh
@@ -11,7 +11,7 @@ if [ -z "${COMPONENT}" ]; then
 fi
 
 INSTANCE_CREATE() {
-INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].State.Name|xargs -n1)
+INSTANCE_STATE=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].State.Name|xargs -n1)
 if [ "${INSTANCE_STATE}" = "running" ]; then
     echo "Instance already created and running"
     exit 0
@@ -21,14 +21,14 @@ if [ "${INSTANCE_STATE}" = "running" ]; then
 fi
 LAUNCH_TEMPLATE_ID=lt-08bf8a3b7c53f50f8
 VERSION=3
-aws ec2 run-instances --launch-template LaunchTemplateId=${LAUNCH_TEMPLATE_ID},Version=${VERSION} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"|jq
+aws ec2 --region us-east-1 run-instances --launch-template LaunchTemplateId=${LAUNCH_TEMPLATE_ID},Version=${VERSION} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"|jq
 sleep 20
 DNS_UPDATE
 
 }
 
 DNS_UPDATE() {
-PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].PrivateIpAddress|xargs -n1)
+PRIVATEIP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].PrivateIpAddress|xargs -n1)
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id ${HOSTZONE_ID} --change-batch file:///tmp/record.json | jq
 }
--- ansible/terminate.sh
+++ ansible/terminate.sh
@@ -10,14 +10,14 @@ if [ "${1}" == "all"  ]; then
     TERMINATE_INSTANCE
 fi
 
-SPOT_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].SpotInstanceRequestId|xargs -n1)
+SPOT_INSTANCE_ID=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].SpotInstanceRequestId|xargs -n1)
 if [ -z "${SPOT_INSTANCE_ID}" ]; then
   echo "component name is wrong or empty"
   exit 1
 fi
 
 TERMINATE_INSTANCE() {
-aws ec2 cancel-spot-instance-requests --spot-instance-request-ids ${SPOT_INSTANCE_ID}
+aws --region us-east-1 ec2 cancel-spot-instance-requests --spot-instance-request-ids ${SPOT_INSTANCE_ID}
 INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].InstanceId|xargs -n1)
-aws ec2 terminate-instances --instance-ids ${INSTANCE_ID}
+aws --region us-east-1 ec2 terminate-instances --instance-ids ${INSTANCE_ID}
 }
